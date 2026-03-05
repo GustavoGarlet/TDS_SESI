7443474e0aa6a06a0bb4729e0291a4fb3775eb98
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-03-05 11:49:36 -0300

    organização das pastas e meu 1°projeto

---
+++ meu projetos/whileSSSS.js
@@ -0,0 +1,47 @@
+{
+    let contador = 0 ;
+        while (contador<10) {
+        contador++;
+        console.log(contador);
+    }
+    let itemRestante = 10;
+        while(itemRestante > 0){
+            itemRestante--;
+            console.log(itemRestante);
+    }    
+}
+
+            console.log("-------------------------------------");
+
+{
+    let i;
+        for (let i = 0; i < 10; i++) {
+        console.log(i);
+    
+    }
+
+        for(let i = 10; i >0; i--){
+        console.log(i);
+    }
+
+}
+
+            console.log("-------------------------------------");
+
+{
+    let i = 0;
+    do{
+        console.log(i);
+        i++;
+    }while(i<10){
+        i+=1
+    }
+
+    let i1 = 10; 
+    do{
+        console.log(i1);
+        i1--;
+    }while (i1>0) {
+        i1 -=2;
+    }
+} //Eu fiz !!!
