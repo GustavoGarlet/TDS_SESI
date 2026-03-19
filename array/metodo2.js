6bf3f6b573fd3d6997d8f71fa55b6b4743841138
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-03-19 11:31:32 -0300

    Metdos de array

---
+++ array/metodo2.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 8];
+
+//Desafio remover o ultimo elemnto
+//2 min pra realizar
+
+numeros.pop();
+console.log(numeros);
+numeros.splice(1, 2); //Remove o indice informado
+console.log(numeros);
