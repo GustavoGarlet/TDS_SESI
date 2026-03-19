ce13e06af83b82fd8fd9fbfd83dd7a9802888a1a
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-03-19 11:32:07 -0300

    Teste de funções

---
+++ funcoes/sem_funcoes.js
@@ -0,0 +1,9 @@
+//sintaxe da função
+/*
+function nomeDaFuncao (parâmetros){
+    //passos a seremexecutados
+}
+*/
+
+const produto1 = "Notebook";
+const preco = 3500;
---
+++ meu projetos/funcoes_teste.js
@@ -0,0 +1,21 @@
+function somar(a, b) {
+    return a + b;
+}
+function subracao(a, b) {
+    return a - b;
+}
+function multiplicacao(a, b) {
+    return a * b;
+}
+function divisao(a, b) {
+    return a / b;
+}
+function IMC(peso, altura) {
+    return peso / (altura * altura);
+}
+
+console.log(somar(5, 7));
+console.log(subracao(30, 10));
+console.log(multiplicacao(5, 7));
+console.log(divisao(14, 7));
+console.log(IMC(56, 1.4));
