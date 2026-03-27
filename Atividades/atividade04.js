170547151227d00dd6363f606e78dad6774b7f98
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-03-27 11:37:31 -0300

    Atividade 04

--- Atividades/atividade04.js
+++ Atividades/atividade04.js
@@ -8,8 +8,8 @@ const produto = {
 let desconto = 0.10
 let precoDesconto;
 
-precoDesconto = produto.preco * desconto;
+precoDesconto = produto.preco * desconto; // Calcula o desconto
 
-let precoFinal = produto.preco > 100  ? `O ${produto.nome} teve um desconto de ${desconto*100}% e agora está por R$ ${produto.preco - precoDesconto} reais.` : 
-                                        `O ${produto.nome} não passou de R$ 100 e por isso continua o preço normal R$ ${produto.preco} reais.`;
+let precoFinal = produto.preco > 100  ? `O ${produto.nome} teve um desconto de ${desconto*100}% e agora está por R$ ${produto.preco - precoDesconto} reais.` : //Se o valor for acima de 100 reais tem um desconto de 10%
+                                        `O ${produto.nome} não passou de R$ 100 e por isso continua o preço normal R$ ${produto.preco} reais.`;  //Se o valor for abaixo de 100 reais permanece o valor inicial
 console.log(precoFinal)
