1fd481ca7ff1acdd65b6f611723f1777f5242368
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-03-27 11:17:37 -0300

    Atividade 02

---
+++ Atividades/atividade02.js
@@ -0,0 +1,8 @@
+let idadeCachorro = 2;
+let idadeHumano = 7;
+idadeHumano = idadeHumano * idadeCachorro;
+
+console.log(`Enquanto o cachorro tem ${idadeCachorro} anos, um Humano tem ${idadeHumano} anos.`);
+if (idadeCachorro > 7){
+    console.log ("O cachorro ta no fim da vida.");
+}
