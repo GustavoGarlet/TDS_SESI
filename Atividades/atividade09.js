a28ae631d474c1616acecc27ade6371f37ad917b
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-04-02 11:24:40 -0300

    atividade09

---
+++ Atividades/atividade09.js
@@ -0,0 +1,15 @@
+function exibir(horaAtual, nome) {
+    if (horaAtual >= 12 && horaAtual < 18){
+        console.log(`Boa tarde ${nome}!!`);
+    } else if (horaAtual >= 18 && horaAtual < 24){
+        console.log(`Boa noite ${nome}!!`);
+    } else if (horaAtual > 24){
+        console.log(`Hora invalida ${nome}, por favor insira outra!!`);
+    } else {
+        console.log(`Bom dia ${nome}!!`);
+    }
+}
+exibir(10, "Gustavo")
+
+
+
