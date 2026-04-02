1402b529cdd13ebee4653fc45b13be725be505d8
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-04-02 11:39:42 -0300

    falta a D e C

---
+++ Atividades/atividade10.js
@@ -0,0 +1,16 @@
+//a
+const nome = []
+
+nome [0] = "sdd 512";
+nome [1] = "placa de video rtx 5070 TI";
+nome [2] = "placa mae b550-m";
+nome [3] = "mamoria ram 16gb";
+nome [4] = "fonte 650W";
+
+//b
+nome.push("mouse attack shark x11");
+
+console.log(nome[2])
+
+
+//c
