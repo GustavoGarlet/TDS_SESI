987fd081f80805007ac658a22c40a31144867b08
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-04-09 11:56:19 -0300

    Atividade 11

---
+++ Atividades/atividade11.js
@@ -0,0 +1,13 @@
+const lista = {
+    nome: "Gustavo",
+    nome: "Jorge",
+    nome: "Lucas",
+    nome: "João",
+    nome: "Simone",
+    nome: "Eloa",
+    nome: "Maria",
+    nome: "sarah"
+}
+console.log()
+
+
