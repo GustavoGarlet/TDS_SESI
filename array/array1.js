7d1861fbc778c2fef217fa503a5bb4c8964940ff
GustavoGarlet <gustavo.stormoski01114797@sesisenaipr.org.br>
2026-04-23 11:16:12 -0300

    array1

--- array/array1.js
+++ array/array1.js
@@ -1,11 +1,11 @@
-// indicea         0         1            2          3 
-const frutas = ["Abacaxi", "Laramnja", "Banana", "Melancia"];
+// Índices           0           1          2        3
+const frutas = ["Abacaxi", "Laranja", "Banana", "Melancia"];
 console.log(frutas[0]);
 console.log(frutas[1]);
 console.log(frutas[2]);
 console.log(frutas[3]);
 console.log(frutas);
-console.log(frutas.length); // Quantidade de indices dentro do array
+console.log(frutas.length); // Quantidade de índices dentro do array
 
-frutas[3] = "Pera"
+frutas[4] = "Morango"
 console.log(frutas);
